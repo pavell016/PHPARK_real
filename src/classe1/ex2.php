e937312eac6a0d9b3f008fabe6505f472e26823a
pavell <pavellupan229@gmail.com>
2025-09-25 15:24:03 +0200

    exercicisphp
    falten exercicis

---
+++ src/classe1/ex2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+echo $_SERVER['HTTP_USER_AGENT'];
+echo $_SERVER['HTTP_HOST']
+?>
+</body>
+</html>
---
+++ src/classe1/ex3.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    $numero=5;
+    $texto="Hola";
+$boleano=false;
+$arr=array(1,2,3,4,5);
+echo var_dump($numero)."<br>";
+echo var_dump($texto)."<br>";
+echo var_dump($boleano)."<br>";
+echo var_dump($arr)."<br>";
+?>
+</body>
+</html>
