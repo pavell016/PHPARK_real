67eb330db7e5347a617aca197c5b950a732747fe
pavell <pavellupan229@gmail.com>
2025-09-30 11:58:28 +0200

    exercicis classe 1

---
+++ src/classe1/ex8.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+// Número del 1 al 7
+$numero = 3;
+
+switch ($numero) {
+    case 1:
+        echo "Lunes";
+        break;
+    case 2:
+        echo "Martes";
+        break;
+    case 3:
+        echo "Miércoles";
+        break;
+    case 4:
+        echo "Jueves";
+        break;
+    case 5:
+        echo "Viernes";
+        break;
+    case 6:
+        echo "Sábado";
+        break;
+    case 7:
+        echo "Domingo";
+        break;
+    default:
+        echo "Número inválido. Debe ser entre 1 y 7.";
+}
+?>
+
+</body>
+</html>
